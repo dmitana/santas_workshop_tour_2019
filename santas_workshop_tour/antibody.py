f0d15587a4683c53be1b95971a2d47dc7ddb17f9
Denis Mitana <denis.mitana1@gmail.com>
2020-03-29 20:51:15 +0200

    Update generating of solution

--- santas_workshop_tour/antibody.py
+++ santas_workshop_tour/antibody.py
@@ -6,39 +6,54 @@ class Antibody:
     This class represents one solution to the Santa's Workshop 2019
     problem.
     """
-
-    def __init__(self, families):
+    def __init__(self):
         """
-        Generate random solution. ID of day, number of people and IDs of
-        all families scheduled for given day will be stored.
-
-        :param families: DataFrame, contains size and preferences of all
-            families.
+        Create a new object of class `Antibody`.
         """
-        days = {}
-        days_under_limits = {}
-        len_days_under_limits = 100
+        self.families = None
+        self.days = None
 
-        # Initialize days dictionary
-        for i in range(1, 101):
-            # Day ID, num of people, IDs of families
-            days[i] = {'id': i, 'size': 0, 'families': []}
+    def generate_solution(self, df_families):
+        """
+        Generate random solution.
+
+        Target day for each family will be stored to `self.families`,
+        where index represent the family.
+
+        Number of people and IDs of all families scheduled for given day
+        will be stored to `self.days`. Index represent the day.
+
+        :param df_families: DataFrame, contains size and preferences of
+            all families.
+        """
+        n_families, n_days = 5000, 100
+        families = np.empty(n_families, dtype=object)
+        days = np.empty(n_days, dtype=object)
+        days_under_limits = np.ones(n_days, dtype=np.bool)
+
+        # Initialize days
+        for i in range(n_days):
+            # Num of people, IDs of families
+            days[i] = {'size': 0, 'families': []}
             days_under_limits[i] = i
 
         # Generate random day for each family
-        for i in range(1, 5001):
-            family_size = families[families.family_id == i]['n_people']
+        for i in range(n_families):
+            family_size = df_families.iloc[i]['n_people']
             while True:
                 # IF branch makes sure only days under limit are picked
-                if len_days_under_limits > 0:
-                    day = np.random.randint(0, len_days_under_limits + 1)
-                    day = list(days_under_limits.values())[day]
+                if days_under_limits.sum() > 0:
+                    day = np.random.randint(0, days_under_limits.sum())
+                    day = np.nonzero(days_under_limits)[0][day]
                 else:
-                    day = np.random.randint(0, 101)
-                if days[day]['size'] + family_size <= 300:
+                    day = np.random.randint(0, n_days)
+                if (days[day]['size'] + family_size) <= 300:
                     days[day]['size'] += family_size
                     days[day]['families'].append(i)
-                    if len_days_under_limits > 0 & days[day]['size'] > 125:
-                        len_days_under_limits -= 1
-                        del days_under_limits[day]
+                    families[i] = day
+                    if days_under_limits.sum() > 0 and days[day]['size'] > 125:
+                        days_under_limits[day] = False
+                    break
+
+        self.families = families
         self.days = days
