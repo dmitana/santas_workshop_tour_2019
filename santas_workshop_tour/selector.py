4f5b359120a058551ad023b499a2786b36c5d56c
Miroslav Sumega <miro.sumega@gmail.com>
2020-04-11 17:22:28 +0200

    Add BasicSelector

--- santas_workshop_tour/selector.py
+++ santas_workshop_tour/selector.py
@@ -3,6 +3,10 @@ from abc import ABC, abstractmethod
 
 class Selector(ABC):
     """Selector abstract class."""
+
+    def __init__(self, affinity_threshold=None):
+        self.affinity_threshold = affinity_threshold
+
     @abstractmethod
     def select(self, population):
         """
@@ -12,3 +16,26 @@ class Selector(ABC):
         :return: list, list of `Antibody` objects.
         """
         pass
+
+
+class BasicSelector(Selector):
+    """
+    Basic Selector implementation.
+    """
+
+    def select(self, population):
+        """
+        Select new population from given `population`.
+
+        Members in population are selected if their affinity is not
+        larger than specified threshold.
+
+        :param population: list, list of `Antibody` objects
+        :return: list, list of `Antibody` objects
+        """
+        return list(
+            filter(
+                lambda x: x.affinity_value <= self.affinity_threshold,
+                population
+            )
+        )
